a7681b8c3ef17e78ddd74837a4e036df133fb281
Carolina Diniz <caroldinizc21@gmail.com>
2025-04-21 11:25:33 -0300

    finalização do componente heading e tipagem ts de children (ReactNode)

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,26 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { TimerIcon } from "lucide-react";
+import { Heading } from "./components/Heading";
+
+import "./styles/global.css";
+import "./styles/theme.css";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+      <Heading>
+        Olá mundo 1
+        <button>
+          <TimerIcon />
         </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
+      </Heading>
+      <p>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quae
+        temporibus possimus consectetur aliquid eius non commodi iusto.
+        Accusamus modi voluptas alias nam. Omnis ipsam minima explicabo
+        repellendus iure deleniti deserunt.
       </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ src/components/Heading.tsx
@@ -0,0 +1,10 @@
+import { ReactNode } from "react";
+import styles from "./Heading.module.css";
+
+type HeadingProps = {
+  children: ReactNode;
+};
+
+export function Heading({ children }: HeadingProps) {
+  return <h1 className={styles.heading}>{children}</h1>;
+}
